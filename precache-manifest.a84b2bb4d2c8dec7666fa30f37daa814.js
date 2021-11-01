abd7d822eebef686ccbf7a0255c8221e36ae46c0
prajwal208 <76072862+prajwal208@users.noreply.github.com>
2021-11-01 15:07:06 +0530

    Updates

---
+++ precache-manifest.a84b2bb4d2c8dec7666fa30f37daa814.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd38b109b5579bd5fc2ff806f9bda7f",
+    "url": "/Blockchain-dBank/index.html"
+  },
+  {
+    "revision": "1eaa3d983b1afd6120f3",
+    "url": "/Blockchain-dBank/static/css/2.0c17ed4c.chunk.css"
+  },
+  {
+    "revision": "ce5809ab4c375cc4fd41",
+    "url": "/Blockchain-dBank/static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "1eaa3d983b1afd6120f3",
+    "url": "/Blockchain-dBank/static/js/2.a8f4bdab.chunk.js"
+  },
+  {
+    "revision": "9143a7c0cbfa518ce39da65db7eaf56a",
+    "url": "/Blockchain-dBank/static/js/2.a8f4bdab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5809ab4c375cc4fd41",
+    "url": "/Blockchain-dBank/static/js/main.fb782110.chunk.js"
+  },
+  {
+    "revision": "4a19e8fbe0f731e0bb1a",
+    "url": "/Blockchain-dBank/static/js/runtime-main.82e5992f.js"
+  },
+  {
+    "revision": "adca41d3777e83416e7ad919fa0da8ac",
+    "url": "/Blockchain-dBank/static/media/dbank.adca41d3.png"
+  }
+]);
